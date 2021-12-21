0dca4a80746861b898c58063a67aba2a90d1c781
Ken Dong <yid164@mail.usask.ca>
2021-12-21 15:22:54 -0600

    Initial Commit

---
+++ iOS-carfax/iOS_carfaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_carfaxApp.swift
+//  iOS-carfax
+//
+//  Created by Yinsheng Dong on 2021-12-21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_carfaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
